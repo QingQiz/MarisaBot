e7ba5aaa35cba0113765e5af3ff74e0fa4f60667
QingQiz <sofeeys@outlook.com>
2021-12-13 20:24:19 +0800

    更新了 peek 的逻辑，允许通过命令禁用peek

--- QQBOT.Core/Plugin/Peek.cs
+++ QQBOT.Core/Plugin/Peek.cs
@@ -1,14 +1,14 @@
-﻿using System.Drawing;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using QQBOT.Core.MiraiHttp;
 using QQBOT.Core.MiraiHttp.Entity;
-using QQBOT.Core.Plugin.PluginEntity;
 using QQBOT.Core.Util;
 
 namespace QQBOT.Core.Plugin
 {
     public class Peek : PluginBase
     {
+        private bool _peekEnabled = true;
         private string CaptureScreen(bool hide=true)
         {
             const int w = 1440;
@@ -26,28 +26,38 @@ namespace QQBOT.Core.Plugin
                 : bitmap.ToB64();
         }
 
-        protected override async Task<PluginTaskState> GroupMessageHandler(MiraiHttpSession session, Message message)
+#pragma warning disable 1998
+        protected override async IAsyncEnumerable<MessageChain> MessageHandler(Message message, MiraiMessageType type)
         {
-            if (message.MessageChain!.PlainText.Trim() == ":peek" && message.At(session.Id))
+            const long authorId = 642191352L;
+
+            var msg      = message.MessageChain!.PlainText.Trim();
+            var senderId = message.Sender!.Id;
+
+            if (!msg.StartsWith(":peek"))
             {
-                await session.SendGroupMessage(new Message(MessageChain.FromBase64(CaptureScreen())),
-                    message.GroupInfo!.Id, message.Source.Id);
-                return PluginTaskState.CompletedTask;
+                yield return null;
             }
 
-            return PluginTaskState.NoResponse;
-        }
-
-        protected override async Task<PluginTaskState> FriendMessageHandler(MiraiHttpSession session, Message message)
-        {
-            if (message.MessageChain!.PlainText.Trim() == ":peek")
+            switch (msg.TrimStart(":peek").Trim())
             {
-                await session.SendFriendMessage(new Message(MessageChain.FromBase64(CaptureScreen(hide: message.Sender!.Id != 642191352L))),
-                    message.Sender!.Id);
-                return PluginTaskState.CompletedTask;
+                // disable peek
+                case "0" when senderId == authorId:
+                    _peekEnabled = false;
+                    yield return MessageChain.FromPlainText("peek disabled");
+                    break;
+                case "1" when senderId == authorId:
+                    _peekEnabled = true;
+                    yield return MessageChain.FromPlainText("peek enabled");
+                    break;
+                case "" when _peekEnabled:
+                    yield return MessageChain.FromBase64(CaptureScreen(senderId != authorId));
+                    break;
+                default:
+                    yield return MessageChain.FromPlainText("Denied");
+                    break;
             }
-
-            return PluginTaskState.NoResponse;
         }
+#pragma warning restore 1998
     }
 }
