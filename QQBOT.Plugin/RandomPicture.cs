3ef8290e8cac0e3685141e88d18332088baa1ccd
QingQiz <sofeeys@outlook.com>
2022-01-14 16:13:08 +0800

    不用 FileSystemWatcher，不好用

--- QQBOT.Plugin/RandomPicture.cs
+++ QQBOT.Plugin/RandomPicture.cs
@@ -7,36 +7,14 @@ using QQBot.Plugin.Shared.Util;
 
 namespace QQBot.Plugin;
 
-[MiraiPluginCommand("抽图")]
-public class RandomPicture: MiraiPluginBase
+[MiraiPluginCommand("抽图", "ct")]
+public class RandomPicture : MiraiPluginBase
 {
-    private static readonly List<string> ImageList = new ();
-    private static FileSystemWatcher? _imageListWatcher;
+    private static readonly List<string> ImageList = new();
 
-    [MiraiPluginCommand(strict: true)]
-    private MiraiPluginTaskState Handler(Message m, MessageSenderProvider p)
+    [MiraiPluginCommand(true, "")]
+    private static MiraiPluginTaskState Handler(Message m, MessageSenderProvider p)
     {
-        if (_imageListWatcher == null)
-        {
-            _imageListWatcher = new FileSystemWatcher
-            {
-                Path         = @"C:\Users\sofee\Desktop\pic",
-                NotifyFilter = NotifyFilters.LastWrite,
-                Filter       = "*.*"
-            };
-
-            _imageListWatcher.Changed += (_, _) =>
-            {
-                lock (ImageList)
-                {
-                    Thread.Sleep(500);
-                    ImageList.Clear();
-                }
-            };
-            _imageListWatcher.EnableRaisingEvents = true;
-            GC.KeepAlive(_imageListWatcher);
-        }
-
         lock (ImageList)
         {
             if (ImageList.Count == 0)
@@ -49,6 +27,28 @@ public class RandomPicture: MiraiPluginBase
             p.Reply(ImageMessage.FromBase64((Image.FromFile(ImageList.RandomTake()) as Bitmap)!.ToB64()), m, false);
 
             return MiraiPluginTaskState.CompletedTask;
+        }
+    }
+
+    [MiraiPluginCommand(true, "reset")]
+    private static MiraiPluginTaskState Reset(Message m, MessageSenderProvider p)
+    {
+        lock (ImageList)
+        {
+            const long authorId = 642191352L;
+            var        sender   = m.Sender!.Id;
+
+            if (sender == authorId)
+            {
+                ImageList.Clear();
+                p.Reply("Success", m);
+                return MiraiPluginTaskState.CompletedTask;
+            }
+            else
+            {
+                p.Reply("Denied", m);
+                return MiraiPluginTaskState.CompletedTask;
+            }
         }
     }
 }
