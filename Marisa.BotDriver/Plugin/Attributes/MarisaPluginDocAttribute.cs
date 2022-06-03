fa82e76614d7490698788e4860f811a2ca0e54c4
QingQiz <sofeeys@outlook.com>
2022-06-04 00:16:39 +0800

    FEAT: 添加了 Doc 属性，现在可以利用反射添加插件帮助文档

---
+++ Marisa.BotDriver/Plugin/Attributes/MarisaPluginDocAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Marisa.BotDriver.Plugin.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false)]
+public class MarisaPluginDocAttribute: Attribute
+{
+    public readonly string Doc;
+
+    public MarisaPluginDocAttribute(string doc)
+    {
+        Doc = doc;
+    }
+}
