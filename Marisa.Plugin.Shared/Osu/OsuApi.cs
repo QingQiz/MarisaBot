d36f18120397a092c775b73683709e2188d1e70e
QINGQIZ <sofeeys@outlook.com>
2022-12-02 14:53:35 +0800

    XXX: 下图失败的重试机制

--- Marisa.Plugin.Shared/Osu/OsuApi.cs
+++ Marisa.Plugin.Shared/Osu/OsuApi.cs
@@ -141,40 +141,54 @@ public static class OsuApi
         ServerCertificateCustomValidationCallback = (_, _, _, _) => true
     });
 
-    public static async Task<string> DownloadBeatmap(long beatmapId, string path)
+    public static async Task<string> DownloadBeatmap(long beatmapId, string path, int retry = 10)
     {
-        using var request = new HttpRequestMessage(new HttpMethod("GET"), $"https://dl.sayobot.cn/beatmaps/download/mini/{beatmapId}");
-        request.Headers.TryAddWithoutValidation("authority", "dl.sayobot.cn");
-        request.Headers.TryAddWithoutValidation("accept",
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-        request.Headers.TryAddWithoutValidation("accept-language", "zh-CN,zh;q=0.9,en-GB;q=0.8,en;q=0.7");
-        request.Headers.TryAddWithoutValidation("sec-ch-ua", "\"Google Chrome\";v=\"105\", \"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"105\"");
-        request.Headers.TryAddWithoutValidation("sec-ch-ua-mobile", "?0");
-        request.Headers.TryAddWithoutValidation("sec-ch-ua-platform", "\"Windows\"");
-        request.Headers.TryAddWithoutValidation("sec-fetch-dest", "document");
-        request.Headers.TryAddWithoutValidation("sec-fetch-mode", "navigate");
-        request.Headers.TryAddWithoutValidation("sec-fetch-site", "none");
-        request.Headers.TryAddWithoutValidation("sec-fetch-user", "?1");
-        request.Headers.TryAddWithoutValidation("upgrade-insecure-requests", "1");
-        request.Headers.TryAddWithoutValidation("user-agent",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36");
+        async Task<string> DownloadBeatmapInner()
+        {
+            using var request = new HttpRequestMessage(new HttpMethod("GET"), $"https://dl.sayobot.cn/beatmaps/download/mini/{beatmapId}");
+            request.Headers.TryAddWithoutValidation("authority", "dl.sayobot.cn");
+            request.Headers.TryAddWithoutValidation("accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            request.Headers.TryAddWithoutValidation("accept-language", "zh-CN,zh;q=0.9,en-GB;q=0.8,en;q=0.7");
+            request.Headers.TryAddWithoutValidation("sec-ch-ua", "\"Google Chrome\";v=\"105\", \"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"105\"");
+            request.Headers.TryAddWithoutValidation("sec-ch-ua-mobile", "?0");
+            request.Headers.TryAddWithoutValidation("sec-ch-ua-platform", "\"Windows\"");
+            request.Headers.TryAddWithoutValidation("sec-fetch-dest", "document");
+            request.Headers.TryAddWithoutValidation("sec-fetch-mode", "navigate");
+            request.Headers.TryAddWithoutValidation("sec-fetch-site", "none");
+            request.Headers.TryAddWithoutValidation("sec-fetch-user", "?1");
+            request.Headers.TryAddWithoutValidation("upgrade-insecure-requests", "1");
+            request.Headers.TryAddWithoutValidation("user-agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36");
 
-        var response = await HttpClient.SendAsync(request);
+            var response = await HttpClient.SendAsync(request);
 
-        var filename = (HttpUtility.ParseQueryString(request.RequestUri!.Query).Get("filename") ?? beatmapId.ToString()) + ".osz";
+            var filename = (HttpUtility.ParseQueryString(request.RequestUri!.Query).Get("filename") ?? beatmapId.ToString()) + ".osz";
 
-        var beatmapPath = Path.Join(path, filename);
+            var beatmapPath = Path.Join(path, filename);
 
-        var s  = await response.Content.ReadAsStreamAsync();
-        var fs = File.OpenWrite(beatmapPath);
+            var s  = await response.Content.ReadAsStreamAsync();
+            var fs = File.OpenWrite(beatmapPath);
 
-        await s.CopyToAsync(fs);
+            await s.CopyToAsync(fs);
 
-        s.Close();
-        fs.Close();
+            s.Close();
+            fs.Close();
 
-        return beatmapPath;
+            return beatmapPath;
+        }
+
+        try
+        {
+            return await DownloadBeatmapInner();
+        }
+        catch (FlurlHttpException)
+        {
+            if (retry == 0) throw;
+            return await DownloadBeatmap(beatmapId, path, retry - 1);
+        }
     }
+
 
     public enum OsuScoreType
     {
