bb81205ba78fc2c2eacd94ccef45e892814ac3c8
QingQiz <sofeeys@outlook.com>
2023-07-19 11:20:38 +0800

    XXX: vue-cli-service build speed up

--- Marisa.Frontend/vue.config.js
+++ Marisa.Frontend/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    filenameHashing: false
+    filenameHashing: false,
+    productionSourceMap: false,
 })
