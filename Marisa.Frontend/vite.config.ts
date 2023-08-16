89fb3be849d8e74e68e6b5839a5ffa09b43e4e3f
QingQiz <sofeeys@outlook.com>
2023-08-16 11:08:21 +0800

    XXX: vue cli --> vite

---
+++ Marisa.Frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': '/src',
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `js/[name].js`,
+                chunkFileNames: `js/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            },
+        },
+    },
+});
