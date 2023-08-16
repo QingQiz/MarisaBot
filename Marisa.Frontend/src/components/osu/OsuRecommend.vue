e2ada0d44b88586af9aacb71cf7a7779cca0c7b7
QingQiz <sofeeys@outlook.com>
2023-08-16 10:01:55 +0800

    FIX: osu recommend:  mode is not defined

--- Marisa.Frontend/src/components/osu/OsuRecommend.vue
+++ Marisa.Frontend/src/components/osu/OsuRecommend.vue
@@ -2,11 +2,13 @@
     <template v-if="data_fetched">
         <div class="p-10 w-fit" style="background-color: hsl(200, 15%, 15%)" v-if="error_message === ''">
             <div class="grid grid-cols-1 gap-y-10 w-[1020px]">
-                <div v-for="i in d" class="z-50" v-if="mode === '3'">
-                    <mania-recommend-card :data="i as ManiaRecommendData" />
-                </div>
-                <div v-for="i in d" class="z-50" v-else>
-                    <osu-recommend-card :data="i as OsuRecommendData" />
+                <div v-for="i in d" class="z-50">
+                    <template v-if="(i as ManiaRecommendData).keyCount !== undefined">
+                        <mania-recommend-card :data="i as ManiaRecommendData" />
+                    </template>
+                    <template v-else>
+                        <osu-recommend-card :data="i as OsuRecommendData" />
+                    </template>
                 </div>
             </div>
         </div>
@@ -22,20 +24,20 @@ import { ManiaRecommendData, OsuRecommendData, RecommendData } from "@/component
 import ManiaRecommendCard from "@/components/osu/partial/ManiaRecommendCard.vue";
 import OsuRecommendCard from "@/components/osu/partial/OsuRecommendCard.vue";
 import axios from "axios";
-import { osu_getRecommend, context_get } from "@/GlobalVars";
+import { context_get } from "@/GlobalVars";
 import { useRoute } from "vue-router";
 
 
 const route = useRoute()
 
-let id   = ref(route.query.id)
+let id = ref(route.query.id)
 
-const data_fetched  = ref(true)
+const data_fetched = ref(true)
 const error_message = ref('')
 
 let d = ref([] as RecommendData[])
 
-axios.get(context_get, { params: { id: id.value, name: "recommend" }})
+axios.get(context_get, { params: { id: id.value, name: "recommend" } })
     .then(data => {
         if (data.data.success === true) {
             d.value = data.data.data.list
