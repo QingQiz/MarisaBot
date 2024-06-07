11a4016d63166f964510a421cdefc4e177fd54f6
QingQiz <sofeeys@outlook.com>
2024-06-07 23:23:46 +0800

    XXX: 优化puppeteer的逻辑

--- Marisa.Utils/WebApi.cs
+++ Marisa.Utils/WebApi.cs
@@ -34,34 +34,23 @@ public static class WebApi
         }
     }
 
-    private static IPage Page
-    {
-        get
-        {
-            var page = Browser.NewPageAsync().Result;
-            page.BringToFrontAsync().Wait();
-            page.SetViewportAsync(new ViewPortOptions
-            {
-                Width = 1, Height = 1
-            });
-            return page;
-        }
-    }
-
     private static async Task<string> RenderUrl(string url)
     {
-        await using var page = Page;
+        await using var page = await Browser.NewPageAsync();
 
-        await page.GoToAsync(url);
-        await page.WaitForNetworkIdleAsync(NetworkIdleOptions);
+        await page.SetViewportAsync(new ViewPortOptions
+        {
+            Width = 1, Height = 1
+        });
+
+        await page.GoToAsync(url, new NavigationOptions
+        {
+            WaitUntil = [WaitUntilNavigation.Networkidle0, WaitUntilNavigation.Load],
+            Timeout   = 30,
+        });
+
         return await page.ScreenshotBase64Async(ScreenshotOptions);
     }
-
-    private static WaitForNetworkIdleOptions NetworkIdleOptions => new()
-    {
-        Timeout  = 0,
-        IdleTime = 1000
-    };
 
     private static ScreenshotOptions ScreenshotOptions => new()
     {
@@ -72,59 +61,34 @@ public static class WebApi
 
     public static async Task<string> MaiMaiBest(Guid guid)
     {
-        await using var page = Page;
-
-        await page.GoToAsync(Frontend + "/maimai/best?id=" + guid);
-        await page.WaitForNetworkIdleAsync(NetworkIdleOptions);
-
-        return await page.ScreenshotBase64Async(ScreenshotOptions);
+        return await RenderUrl(Frontend + "/maimai/best?id=" + guid);
     }
 
     public static async Task<string> OsuScore(string name, int modeInt, int? bpRank, bool recent, bool fail)
     {
-        await using var page = Page;
-
-        await page.GoToAsync(Frontend + "/osu/score?" + "name=" + name + "&mode=" + modeInt + "&bpRank=" + (bpRank ?? 1) +
+        return await RenderUrl(Frontend + "/osu/score?" + "name=" + name + "&mode=" + modeInt + "&bpRank=" + (bpRank ?? 1) +
             (recent ? "&recent=" + recent : "") +
             (fail ? "&fail=" + fail : ""));
-        await page.WaitForNetworkIdleAsync(NetworkIdleOptions);
-        return await page.ScreenshotBase64Async(ScreenshotOptions);
     }
 
     public static async Task<string> OsuRecommend(Guid contextId)
     {
-        await using var page = Page;
-
-        await page.GoToAsync(Frontend + "/osu/recommend?id=" + contextId);
-        await page.WaitForNetworkIdleAsync(NetworkIdleOptions);
-        return await page.ScreenshotBase64Async(ScreenshotOptions);
+        return await RenderUrl(Frontend + "/osu/recommend?id=" + contextId);
     }
 
     public static async Task<string> OsuPreview(Guid contextId)
     {
-        await using var page = Page;
-
-        await page.GoToAsync(Frontend + "/osu/preview?id=" + contextId);
-        await page.WaitForNetworkIdleAsync(NetworkIdleOptions);
-        return await page.ScreenshotBase64Async(ScreenshotOptions);
+        return await RenderUrl(Frontend + "/osu/preview?id=" + contextId);
     }
 
     public static async Task<string> MaiMaiRecommend(Guid contextId)
     {
-        await using var page = Page;
-
-        await page.GoToAsync(Frontend + "/maimai/recommend?id=" + contextId);
-        await page.WaitForNetworkIdleAsync(NetworkIdleOptions);
-        return await page.ScreenshotBase64Async(ScreenshotOptions);
+        return await RenderUrl(Frontend + "/maimai/recommend?id=" + contextId);
     }
 
     public static async Task<string> OngekiSong(int id)
     {
-        await using var page = Page;
-
-        await page.GoToAsync(Frontend + $"/ongeki/song/{id}");
-        await page.WaitForNetworkIdleAsync(NetworkIdleOptions);
-        return await page.ScreenshotBase64Async(ScreenshotOptions);
+        return await RenderUrl(Frontend + $"/ongeki/song/{id}");
     }
 
     public static async Task<string> ChunithmSummary(Guid contextId)
