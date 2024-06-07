e968f611008d56eb49abdc633be1135601e48fe6
QingQiz <sofeeys@outlook.com>
2024-06-07 22:58:34 +0800

    FEAT: blacklist: unban

--- Marisa.Plugin/BlackList.cs
+++ Marisa.Plugin/BlackList.cs
@@ -14,9 +14,7 @@ public class BlackList : MarisaPluginBase
     [MarisaPluginTrigger(nameof(MarisaPluginTrigger.AlwaysTrueTrigger))]
     private static async Task<MarisaPluginTaskState> Handler(Message message)
     {
-        if (message.Sender == null) return MarisaPluginTaskState.NoResponse;
-
-        var u = message.Sender!.Id;
+        var u = message.Sender.Id;
 
         if (!_cacheInitialized)
         {
@@ -28,9 +26,13 @@ public class BlackList : MarisaPluginBase
 
         if (Cache.Contains(u)) return MarisaPluginTaskState.CompletedTask;
 
-        if (message.GroupInfo != null)
+        if (message.GroupInfo == null)
         {
-            var db = new BotDbContext();
+            return MarisaPluginTaskState.NoResponse; // 插件不处理这条消息，等于不ban
+        }
+
+        {
+            var db      = new BotDbContext();
             var filters = db.CommandFilters.Where(x => x.GroupId == message.GroupInfo.Id);
 
             foreach (var f in filters)
@@ -40,69 +42,77 @@ public class BlackList : MarisaPluginBase
                     if (message.Command.StartsWith(f.Prefix, StringComparison.OrdinalIgnoreCase))
                     {
                         // 阻断
-                        // message.Reply("被阻止的");
                         return MarisaPluginTaskState.CompletedTask;
                     }
                 }
 
-                if (!string.IsNullOrWhiteSpace(f.Type))
-                {
-                    var t = (MessageDataType)Enum.Parse(typeof(MessageDataType), f.Type);
+                if (string.IsNullOrWhiteSpace(f.Type)) continue;
 
-                    if (message.MessageChain!.Messages.Any(x => x.Type == t))
-                    {
-                        // 阻断
-                        // message.Reply("被阻止的");
-                        return MarisaPluginTaskState.CompletedTask;
-                    }
+                var t = (MessageDataType)Enum.Parse(typeof(MessageDataType), f.Type);
+
+                if (message.MessageChain!.Messages.Any(x => x.Type == t))
+                {
+                    // 阻断
+                    return MarisaPluginTaskState.CompletedTask;
                 }
             }
         }
 
-        return MarisaPluginTaskState.NoResponse;   // 插件不处理这条消息，等于不ban
+        return MarisaPluginTaskState.NoResponse; // 插件不处理这条消息，等于不ban
     }
 
     [MarisaPluginCommand(":ban")]
     private static MarisaPluginTaskState Ban(Message message)
     {
-        if (!ConfigurationManager.Configuration.Commander.Contains(message.Sender!.Id))
+        if (!ConfigurationManager.Configuration.Commander.Contains(message.Sender.Id))
         {
             message.Reply("你没有资格。");
             return MarisaPluginTaskState.CompletedTask;
         }
 
-        if (string.IsNullOrWhiteSpace(message.Command))
+        if (!TryGetId(message, out var qq))
         {
-            var at = message.MessageChain!.Messages.FirstOrDefault(m => m.Type == MessageDataType.At);
+            return MarisaPluginTaskState.CompletedTask;
+        }
 
-            if (at == null)
-            {
-                message.Reply("错误的命令格式");
-            }
-            else
-            {
-                var db = new BotDbContext();
-                var qq = (at as MessageDataAt)!.Target;
-                Cache.Add(qq);
-                db.BlackLists.Add(new EntityFrameworkCore.Entity.BlackList(qq));
-                db.SaveChanges();
-                message.Reply("好了");
-            }
+        using var db = new BotDbContext();
+
+        Cache.Add(qq);
+        db.BlackLists.Add(new EntityFrameworkCore.Entity.BlackList(qq));
+        db.SaveChanges();
+        message.Reply("好了");
+
+        return MarisaPluginTaskState.CompletedTask;
+    }
+
+    [MarisaPluginCommand(":unban")]
+    private static MarisaPluginTaskState UnBan(Message message)
+    {
+        if (!ConfigurationManager.Configuration.Commander.Contains(message.Sender.Id))
+        {
+            message.Reply("你没有资格。");
+            return MarisaPluginTaskState.CompletedTask;
+        }
+
+        if (!TryGetId(message, out var qq))
+        {
+            return MarisaPluginTaskState.CompletedTask;
+        }
+
+        using var db = new BotDbContext();
+
+        var item = db.BlackLists.FirstOrDefault(b => b.UId == qq);
+
+        if (item != null)
+        {
+            Cache.Remove(qq);
+            db.BlackLists.Remove(item);
+            db.SaveChanges();
+            message.Reply("好了");
         }
         else
         {
-            if (long.TryParse(message.Command.Trim(), out var qq))
-            {
-                var db = new BotDbContext();
-                Cache.Add(qq);
-                db.BlackLists.Add(new EntityFrameworkCore.Entity.BlackList(qq));
-                db.SaveChanges();
-                message.Reply("好了");
-            }
-            else
-            {
-                message.Reply("错误的命令格式");
-            }
+            message.Reply("这个人不在黑名单里");
         }
 
         return MarisaPluginTaskState.CompletedTask;
@@ -115,5 +125,22 @@ public class BlackList : MarisaPluginBase
         var db = new BotDbContext();
         message.Reply(string.Join("\n", db.BlackLists.Select(b => b.UId)));
         return MarisaPluginTaskState.CompletedTask;
+    }
+
+    private static bool TryGetId(Message message, out long qq)
+    {
+        var at = message.MessageChain!.Messages.FirstOrDefault(m => m.Type == MessageDataType.At);
+
+        if (at != null) qq = (at as MessageDataAt)!.Target;
+        else if (long.TryParse(message.Command.Trim(), out qq))
+        {
+        }
+        else
+        {
+            message.Reply("错误的命令格式");
+            return false;
+        }
+
+        return true;
     }
 }
