a77a1d74c37a3efdd41d81861b6db4682f4ad20d
QingQiz <sofeeys@outlook.com>
2024-06-07 23:54:45 +0800

    FIX: dialog: 移除发生异常的会话

--- Marisa.Plugin/Dialog.cs
+++ Marisa.Plugin/Dialog.cs
@@ -22,10 +22,9 @@ public class Dialog : MarisaPluginBase
     [MarisaPluginCommand]
     private static MarisaPluginTaskState MessageHandler(Message message)
     {
-        if (message.Sender == null) return MarisaPluginTaskState.NoResponse;
-
         var groupId  = message.GroupInfo?.Id;
-        var senderId = message.Sender!.Id;
+        var senderId = message.Sender.Id;
+
         lock (Handlers)
         {
             if (Handlers.Count == 0) return MarisaPluginTaskState.NoResponse;
@@ -33,33 +32,39 @@ public class Dialog : MarisaPluginBase
             (long?, long?) key = (groupId, senderId);
 
             if (!Handlers.ContainsKey(key)) key = (groupId, null);
-            if (!Handlers.ContainsKey(key)) return MarisaPluginTaskState.NoResponse;
+            if (!Handlers.TryGetValue(key, out var value)) return MarisaPluginTaskState.NoResponse;
 
-            var handler = Handlers[key];
-
-            switch (handler(message).Result)
+            try
             {
-                // 完成了，删除 handler
-                case MarisaPluginTaskState.CompletedTask:
-                    Handlers.Remove(key);
-                    return MarisaPluginTaskState.CompletedTask;
-                // 处理了一部分，但没有完成，不删除，但是终止 event 传播
-                case MarisaPluginTaskState.ToBeContinued:
-                    return MarisaPluginTaskState.CompletedTask;
-                // handler 没处理，交给其它插件处理
-                case MarisaPluginTaskState.NoResponse:
-                    Handlers.Remove(key);
-                    var rep = MessageHandler(message);
-                    Handlers.Add(key, handler);
-                    return rep;
-                // 插件自闭了，请求删除自己
-                case MarisaPluginTaskState.Canceled:
-                    Handlers.Remove(key);
-                    return MessageHandler(message);
-                // 错误的状态，删除这个异常的 handler（虽然不太可能发生）
-                default:
-                    Handlers.Remove((groupId, senderId));
-                    return MessageHandler(message);
+                switch (value(message).Result)
+                {
+                    // 完成了，删除 handler
+                    case MarisaPluginTaskState.CompletedTask:
+                        Handlers.Remove(key);
+                        return MarisaPluginTaskState.CompletedTask;
+                    // 处理了一部分，但没有完成，不删除，但是终止 event 传播
+                    case MarisaPluginTaskState.ToBeContinued:
+                        return MarisaPluginTaskState.CompletedTask;
+                    // handler 没处理，交给其它插件处理
+                    case MarisaPluginTaskState.NoResponse:
+                        Handlers.Remove(key);
+                        var rep = MessageHandler(message);
+                        Handlers.Add(key, value);
+                        return rep;
+                    // 插件自闭了，请求删除自己
+                    case MarisaPluginTaskState.Canceled:
+                        Handlers.Remove(key);
+                        return MessageHandler(message);
+                    // 错误的状态，删除这个异常的 handler（虽然不太可能发生）
+                    default:
+                        Handlers.Remove((groupId, senderId));
+                        return MessageHandler(message);
+                }
+            }
+            catch
+            {
+                Handlers.Remove(key);
+                throw;
             }
         }
     }
