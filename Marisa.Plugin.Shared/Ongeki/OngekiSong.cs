b4b8cb53a651f596633bab7993edbfee9ff9e17e
QingQiz <sofeeys@outlook.com>
2024-07-06 17:32:57 +0800

    FIX: ongeki: level给错了

--- Marisa.Plugin.Shared/Ongeki/OngekiSong.cs
+++ Marisa.Plugin.Shared/Ongeki/OngekiSong.cs
@@ -35,13 +35,21 @@ public record OngekiChartRecord(
 
 public class OngekiSong : Song
 {
-    private string Source;
-    private string Genre;
+    public static readonly Dictionary<string, string> LevelAlias = new()
+    {
+        { "绿", "BASIC" },
+        { "黄", "ADVANCED" },
+        { "红", "EXPERT" },
+        { "紫", "MASTER" },
+        { "白", "Lunatic" }
+    };
     private string BossCard;
     private int BossLevel;
-    private string ReleaseDate;
-    private string CopyRight;
     public List<OngekiChartRecord?> Charts;
+    private string CopyRight;
+    private string Genre;
+    private string ReleaseDate;
+    private string Source;
 
     public OngekiSong(OngekiMusicDataRecord d)
     {
@@ -59,6 +67,7 @@ public class OngekiSong : Song
         Title   = d.Title;
         Artist  = d.Artist;
         Version = d.Version;
+
         Bpm = d.Charts
             .Where(c => c is not null)
             .SelectMany(c => c!.Bpm.Split('\t'))
@@ -69,9 +78,9 @@ public class OngekiSong : Song
             .Select(c => c!.Const));
 
         Levels.AddRange(d.Charts
-            .Select((c, i) => (c, i))
-            .Where(c => c.c is not null)
-            .Select(c => LevelAlias.Values.ElementAt(c.i)));
+            .Where(c => c != null)
+            .Cast<OngekiChartRecord>()
+            .Select(x => $"{(int)x.Const}{(x.Const - (int)x.Const >= 0.5 ? "+" : "")}"));
 
         Charters.AddRange(d.Charts
             .Where(c => c is not null)
@@ -87,15 +96,6 @@ public class OngekiSong : Song
     {
         return ResourceManager.GetCover(Id);
     }
-
-    public static readonly Dictionary<string, string> LevelAlias = new()
-    {
-        { "绿", "BASIC" },
-        { "黄", "ADVANCED" },
-        { "红", "EXPERT" },
-        { "紫", "MASTER" },
-        { "白", "Lunatic" },
-    };
 
     public override string GetImage()
     {
