2fc9294cc9b202146052afeb90888703969c43e2
QINGQIZ <sofeeys@outlook.com>
2025-02-18 01:06:33 +0800

    fix: dialog: dialog设计问题，允许多个dialog

--- Marisa.Plugin/Dialog.cs
+++ Marisa.Plugin/Dialog.cs
@@ -1,4 +1,5 @@
-﻿namespace Marisa.Plugin;
+﻿// ReSharper disable UnusedMember.Local
+namespace Marisa.Plugin;
 
 [MarisaPluginNoDoc]
 [MarisaPlugin(PluginPriority.Dialog)]
@@ -6,15 +7,18 @@
 public class Dialog : MarisaPluginBase
 {
     // map (group id, user id) to handler
-    private static readonly Dictionary<(long?, long?), Shared.Dialog.Dialog.MessageHandler> Handlers = new();
+    private static readonly Dictionary<(long?, long?), Queue<Shared.Dialog.Dialog.MessageHandler>> Handlers = new();
 
     public static bool AddHandler(long? groupId, long? senderId, Shared.Dialog.Dialog.MessageHandler handler)
     {
         lock (Handlers)
         {
-            if (Handlers.ContainsKey((groupId, senderId))) return false;
+            if (!Handlers.ContainsKey((groupId, senderId)))
+            {
+                Handlers.Add((groupId, senderId), new Queue<Shared.Dialog.Dialog.MessageHandler>());
+            }
+            Handlers[(groupId, senderId)].Enqueue(handler);
 
-            Handlers[(groupId, senderId)] = handler;
             return true;
         }
     }
@@ -31,39 +35,41 @@ public class Dialog : MarisaPluginBase
 
             (long?, long?) key = (groupId, senderId);
 
+            // 是否有 针对某个人的dialog
             if (!Handlers.ContainsKey(key)) key = (groupId, null);
+            // 是否有 针对整个群组的dialog
             if (!Handlers.TryGetValue(key, out var value)) return MarisaPluginTaskState.NoResponse;
+            if (value.Count == 0) return MarisaPluginTaskState.NoResponse;
 
             try
             {
-                switch (value(message).Result)
+                var front = value.Peek();
+                switch (front(message).Result)
                 {
                     // 完成了，删除 handler
                     case MarisaPluginTaskState.CompletedTask:
-                        Handlers.Remove(key);
+                        value.Dequeue();
                         return MarisaPluginTaskState.CompletedTask;
                     // 处理了一部分，但没有完成，不删除，但是终止 event 传播
                     case MarisaPluginTaskState.ToBeContinued:
                         return MarisaPluginTaskState.CompletedTask;
                     // handler 没处理，交给其它插件处理
                     case MarisaPluginTaskState.NoResponse:
-                        Handlers.Remove(key);
+                        value.Dequeue();
                         var rep = MessageHandler(message);
-                        Handlers.Add(key, value);
+                        value.Enqueue(front);
                         return rep;
                     // 插件自闭了，请求删除自己
                     case MarisaPluginTaskState.Canceled:
-                        Handlers.Remove(key);
-                        return MessageHandler(message);
                     // 错误的状态，删除这个异常的 handler（虽然不太可能发生）
                     default:
-                        Handlers.Remove((groupId, senderId));
+                        value.Dequeue();
                         return MessageHandler(message);
                 }
             }
             catch
             {
-                Handlers.Remove(key);
+                value.Dequeue();
                 throw;
             }
         }
