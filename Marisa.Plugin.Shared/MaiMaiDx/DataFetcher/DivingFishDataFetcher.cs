4b3e7fa54afb93ef5ef578eaae21eb0e2be5c5ed
QINGQIZ <sofeeys@outlook.com>
2025-02-23 01:56:47 +0800

    misc: mai: 先把ratingrank和拟合难度注释了，以后再说吧

--- Marisa.Plugin.Shared/MaiMaiDx/DataFetcher/DivingFishDataFetcher.cs
+++ Marisa.Plugin.Shared/MaiMaiDx/DataFetcher/DivingFishDataFetcher.cs
@@ -1,24 +1,24 @@
 ﻿using Flurl.Http;
 using Marisa.Plugin.Shared.Util.SongDb;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Marisa.Plugin.Shared.MaiMaiDx.DataFetcher;
 
 public class DivingFishDataFetcher : DataFetcher
 {
-    private readonly Dictionary<int, List<DiffData?>> _diffDict;
-    private readonly List<Rank> _raRankList;
+    // TODO 下面的内容以后再来写吧！
+    // private readonly Dictionary<int, List<DiffData?>> _diffDict;
+    // private readonly List<Rank> _raRankList;
 
     public DivingFishDataFetcher(SongDb<MaiMaiSong> songDb) : base(songDb)
     {
-        _diffDict   = FetchDiffData().Result;
-        _raRankList = FetchRaRankList().Result.OrderByDescending(x => x.Ra).ToList();
+        // _diffDict   = FetchDiffData().Result;
+        // _raRankList = FetchRaRankList().Result.OrderByDescending(x => x.Ra).ToList();
     }
 
     public override async Task<DxRating> GetRating(Message message)
     {
-        var (username, qq) = Chunithm.DataFetcher.DivingFishDataFetcher.AtOrSelf(message, true);
+        var (username, qq) = Chunithm.DataFetcher.DataFetcher.AtOrSelf(message, true);
 
         var rep = await "https://www.diving-fish.com/api/maimaidxprober/query/player".PostJsonAsync(
             username.IsWhiteSpace()
@@ -29,7 +29,7 @@ public class DivingFishDataFetcher : DataFetcher
 
     public override async Task<Dictionary<(long Id, int LevelIdx), SongScore>> GetScores(Message message)
     {
-        var (_, qq) = Chunithm.DataFetcher.DivingFishDataFetcher.AtOrSelf(message, true);
+        var (_, qq) = Chunithm.DataFetcher.DataFetcher.AtOrSelf(message, true);
 
         var response = await "https://www.diving-fish.com/api/maimaidxprober/query/plate".PostJsonAsync(new
         {
@@ -43,39 +43,39 @@ public class DivingFishDataFetcher : DataFetcher
             .ToDictionary(ss => (ss!.Id, ss.LevelIdx))!;
     }
 
-    public DiffData GetFitDiff(int songId, int levelIdx)
-    {
-        return _diffDict[songId][levelIdx] ?? throw new KeyNotFoundException("No data found for this song and level.");
-    }
-
-    public List<int> GetRaRank()
-    {
-        return _raRankList.Select(x => x.Ra).ToList();
-    }
-
-    private async Task<List<Rank>> FetchRaRankList()
-    {
-        var json = await "https://www.diving-fish.com/api/maimaidxprober/rating_ranking".GetStringAsync();
-
-        return JArray.Parse(json).Select(x => x.ToObject<Rank>()).ToList()!;
-    }
-
-    private async Task<Dictionary<int, List<DiffData?>>> FetchDiffData()
-    {
-        var json = await "https://www.diving-fish.com/api/maimaidxprober/chart_stats".GetStringAsync();
-
-        return JObject.Parse(json).SelectToken("$.charts")!.ToObject<Dictionary<int, List<DiffData?>>>()!;
-    }
-
-    private record Rank([JsonProperty("username")] string Username, [JsonProperty("ra")] int Ra);
-
-    public record DiffData(
-        [JsonProperty("cnt")] int PlayCount,
-        [JsonProperty("fit_diff")] double FitDiff,
-        [JsonProperty("avg")] double AvgAchievement,
-        [JsonProperty("avg_dx")] double AvgDxScore,
-        [JsonProperty("std_dev")] double Std,
-        [JsonProperty("dist")] int[] RankCount,
-        [JsonProperty("fc_dist")] int[] FcCount
-    );
+    // public DiffData GetFitDiff(int songId, int levelIdx)
+    // {
+    //     return _diffDict[songId][levelIdx] ?? throw new KeyNotFoundException("No data found for this song and level.");
+    // }
+    //
+    // public List<int> GetRaRank()
+    // {
+    //     return _raRankList.Select(x => x.Ra).ToList();
+    // }
+    //
+    // private async Task<List<Rank>> FetchRaRankList()
+    // {
+    //     var json = await "https://www.diving-fish.com/api/maimaidxprober/rating_ranking".GetStringAsync();
+    //
+    //     return JArray.Parse(json).Select(x => x.ToObject<Rank>()).ToList()!;
+    // }
+    //
+    // private async Task<Dictionary<int, List<DiffData?>>> FetchDiffData()
+    // {
+    //     var json = await "https://www.diving-fish.com/api/maimaidxprober/chart_stats".GetStringAsync();
+    //
+    //     return JObject.Parse(json).SelectToken("$.charts")!.ToObject<Dictionary<int, List<DiffData?>>>()!;
+    // }
+    //
+    // private record Rank([JsonProperty("username")] string Username, [JsonProperty("ra")] int Ra);
+    //
+    // public record DiffData(
+    //     [JsonProperty("cnt")] int PlayCount,
+    //     [JsonProperty("fit_diff")] double FitDiff,
+    //     [JsonProperty("avg")] double AvgAchievement,
+    //     [JsonProperty("avg_dx")] double AvgDxScore,
+    //     [JsonProperty("std_dev")] double Std,
+    //     [JsonProperty("dist")] int[] RankCount,
+    //     [JsonProperty("fc_dist")] int[] FcCount
+    // );
 }
