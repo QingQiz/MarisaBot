ead9f22929d53b97e51c5a23409966ee91cbc474
QINGQIZ <sofeeys@outlook.com>
2025-04-17 21:41:50 +0800

    feat: retrieve: != operator

--- Marisa.Plugin.Shared/Interface/IMarisaPluginWithRetrieve.cs
+++ Marisa.Plugin.Shared/Interface/IMarisaPluginWithRetrieve.cs
@@ -250,6 +250,11 @@ public interface IMarisaPluginWithRetrieve<TSong> where TSong : Song
                         key = e[..i].ToString();
                         op  = "=";
                         break;
+                    case '!' when i + 1 < s.Length && s[i + 1] == '=':
+                        key = e[..i].ToString();
+                        op  = "!=";
+                        i++;
+                        break;
                     default:
                         continue;
                 }
@@ -268,8 +273,8 @@ public interface IMarisaPluginWithRetrieve<TSong> where TSong : Song
                     message.Reply($"{key}不是可用的约束条件，可用的：\n{doc}\n可用命令的非共同前缀替代。");
                     throw new ArgumentOutOfRangeException();
                 case 0: // Charter
-                    CheckOp(op);
-                    diffConstraint.Add((song, levelIdx) => song.Charters[levelIdx].Contains(val, StringComparison.OrdinalIgnoreCase));
+                    CheckEqOp(op);
+                    diffConstraint.Add((song, levelIdx) => MakeEqConstraint(op, song.Charters[levelIdx], val));
                     break;
                 case 1: // Constant
                     if (double.TryParse(val, out var constant))
@@ -301,16 +306,16 @@ public interface IMarisaPluginWithRetrieve<TSong> where TSong : Song
                     message.Reply("Bpm 只能为数字");
                     throw new ArgumentOutOfRangeException();
                 case 3: // Artist
-                    CheckOp(op);
-                    songConstraint.Add(song => song.Artist.Contains(val, StringComparison.OrdinalIgnoreCase));
+                    CheckEqOp(op);
+                    songConstraint.Add(song => MakeEqConstraint(op, song.Artist, val));
                     break;
                 case 4: // Title
-                    CheckOp(op);
-                    songConstraint.Add(song => song.Title.Contains(val, StringComparison.OrdinalIgnoreCase));
+                    CheckEqOp(op);
+                    songConstraint.Add(song => MakeEqConstraint(op, song.Title, val));
                     break;
                 case 5: // Version
-                    CheckOp(op);
-                    songConstraint.Add(song => song.Version.Contains(val, StringComparison.OrdinalIgnoreCase));
+                    CheckEqOp(op);
+                    songConstraint.Add(song => MakeEqConstraint(op, song.Version, val));
                     break;
                 case 6: // Id
                     if (long.TryParse(val, out var id))
@@ -331,8 +336,8 @@ public interface IMarisaPluginWithRetrieve<TSong> where TSong : Song
                     message.Reply("LevelIndex 只能为数字");
                     throw new ArgumentOutOfRangeException();
                 case 8: // DiffName
-                    CheckOp(op);
-                    diffConstraint.Add((song, levelIdx) => song.DiffNames[levelIdx].Contains(val, StringComparison.OrdinalIgnoreCase));
+                    CheckEqOp(op);
+                    diffConstraint.Add((song, levelIdx) => MakeEqConstraint(op, song.DiffNames[levelIdx], val));
                     break;
                 case 9: // Level
                     diffConstraint.Add((song, levelIdx) => LevelComparer(song.Levels[levelIdx], val, op));
@@ -359,6 +364,7 @@ public interface IMarisaPluginWithRetrieve<TSong> where TSong : Song
             {
                 ">=" => (x, y) => Comparer<T>.Default.Compare(x, y) >= 0,
                 "<=" => (x, y) => Comparer<T>.Default.Compare(x, y) <= 0,
+                "!=" => (x, y) => Comparer<T>.Default.Compare(x, y) != 0,
                 "="  => (x, y) => Comparer<T>.Default.Compare(x, y) == 0,
                 ">"  => (x, y) => Comparer<T>.Default.Compare(x, y) > 0,
                 "<"  => (x, y) => Comparer<T>.Default.Compare(x, y) < 0,
@@ -370,9 +376,15 @@ public interface IMarisaPluginWithRetrieve<TSong> where TSong : Song
         {
             if (a.Length == 0 || b.Length == 0) return false;
 
+            switch (op)
+            {
+                case "!=": return a != b;
+                case "=": return a == b;
+            }
+
             if (!b[0].IsDigit() || !a[0].IsDigit())
             {
-                return op == "=" && a.Contains(b, StringComparison.OrdinalIgnoreCase);
+                return false;
             }
 
             var cmp  = GetComparer<int>(op);
@@ -386,12 +398,17 @@ public interface IMarisaPluginWithRetrieve<TSong> where TSong : Song
             return a.Last() == '+' ? cmp(1, 0) : cmp(0, 1);
         }
 
-        void CheckOp(string op)
+        void CheckEqOp(string op)
         {
-            if (op == "=") return;
+            if (op is "=" or "!=") return;
 
-            message.Reply("该约束可用操作符：=");
+            message.Reply("该约束可用操作符 =、!=");
             throw new ArgumentOutOfRangeException();
+        }
+
+        bool MakeEqConstraint(string op, string songProp, string val)
+        {
+            return (x => op == "!=" ? !x : x)(songProp.Contains(val, StringComparison.OrdinalIgnoreCase));
         }
     }
 
