d0300c81d6384c0c102a040eb9b31756ea77a5dd
kali <johndoe@example.com>
2026-05-11 15:56:52 +0800

    feat: enhance Translate plugin with exception handling and improved translation prompt

--- Marisa.Plugin/Translate.cs
+++ Marisa.Plugin/Translate.cs
@@ -1,10 +1,12 @@
+using Flurl.Http;
+using Marisa.Plugin.Shared.Interface;
 using Marisa.Plugin.Shared.Util;
 
 namespace Marisa.Plugin;
 
 [MarisaPluginDoc("翻译文本。", "<文本>")]
 [MarisaPluginCommand("翻译", "translate", "trans")]
-public class Translate : MarisaPluginBase
+public class Translate : MarisaPluginBase, IHandleCommonException
 {
     [MarisaPluginCommand]
     private async Task<MarisaPluginTaskState> Handler(Message message)
@@ -18,12 +20,34 @@ public class Translate : MarisaPluginBase
         }
 
         var translated = await OpenAiClient.Default.ChatAsync(
-            "You are a professional translator. Translate the given text to Chinese. " +
+            "You are a professional translator and native speaker of Chinese. Translate the given text to Chinese. " +
             "Output ONLY the translated text, nothing else.",
             text
         );
 
         message.Reply(translated);
         return MarisaPluginTaskState.CompletedTask;
+    }
+
+    public override Task ExceptionHandler(Exception exception, Message message)
+    {
+        if (CommonExceptionHandler.TryHandleCommonException(exception, message))
+        {
+            return Task.CompletedTask;
+        }
+
+        switch (CommonExceptionHandler.UnwrapCommonException(exception))
+        {
+            case FlurlHttpTimeoutException:
+                message.Reply("超时");
+                break;
+            case FlurlHttpException e:
+                message.Reply(e.Message);
+                break;
+            default:
+                return base.ExceptionHandler(exception, message);
+        }
+
+        return Task.CompletedTask;
     }
 }
